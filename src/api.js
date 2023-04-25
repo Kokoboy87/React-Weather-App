1ea25badb6c3408174833261856476be5f11ccf8
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2023-04-25 00:39:09 -0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+//const url = '/cities'
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': 'fd1c7d1856msh41beab3bc48b0fap1566d5jsn2074fb29bf2b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '5b92ba9727983fb2b2185c5801669226'
